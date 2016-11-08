723774581a8347f7770f14b00d4710bfde2f1bcf
hambuol <oliver.hamburger@ssfs.org>
2016-11-08 08:34:49 -0500

    fin

--- bricks.py
+++ bricks.py
@@ -1,12 +1,28 @@
+# written by oliver hamburger
+# program is a class for creating a brick pyramid
+# last modified 11/8/16
+
+
+# imports needed moduel pygame
 import pygame
 
+
 class Brick:
+    """class that sets basics needed to make a brick pyramid. user can draw a brick"""
     def __init__(self, mainsurface, width):
         self.mainsurface = mainsurface
         self.width = width
         pygame.init()
 
-    def drawbrick(self, x, y):
-        pygame.draw.rect(self.mainsurface, (255, 0, 0), (x, y, self.width,25), 0)
-        pygame.display.update()
+    def drawbrick(self, x, y, r, g, b):
+        """
 
+        :param x: x position of brick
+        :param y: y position of brick
+        :param r: ammount of red color in a brick
+        :param g: ammount of green color in a brick
+        :param b: ammount of blue color in a brick
+        :return: none
+        """
+        pygame.draw.rect(self.mainsurface, (r, g, b), (x, y, self.width, 25), 0)
+        pygame.display.update()
--- brickspicture.py
+++ brickspicture.py
@@ -1,44 +1,53 @@
+# written by oliver hamburger
+# program draws a brick pyramid in pygame
+# last modified 11/8/16
+
+
+# imports needed moduel pygame
 import pygame, sys
 from pygame.locals import *
+# imports bricks class
 import bricks
-
+# sets backround color to white
+backround_c = (255, 255, 255)
+# sets number of brick in row in pyramid
 numberofB = 9
-SPACE = 5
+SPACE_BETWEEN_BRICKS = 5
 
 pygame.init()
-mainSurface = pygame.display.set_mode((500, 500))
+# made variable to pass width of screen size
+W = 650
+# creates surface in pygame, sets caption, and fills the screen color
+mainSurface = pygame.display.set_mode((W, 500))
 pygame.display.set_caption("Bricks")
+mainSurface.fill(backround_c)
 
-
-
-width = (500 - ((numberofB + 1) * SPACE))/numberofB
+# calculates the width of the brick pyramid
+width = (W - ((numberofB + 1) * SPACE_BETWEEN_BRICKS))/numberofB
 brick = bricks.Brick(mainSurface, width)
-
-xpos = SPACE
-for x in range(numberofB):
-    brick.drawbrick(xpos, 470, 255, 0, 0)
-    xpos += width + SPACE
-
-xpos = SPACE + width + 5
-for x in range(numberofB - 2):
-    brick.drawbrick(xpos, 440, 200, 125, 40)
-    xpos += width + SPACE
-
-xpos = SPACE + width * 2 + 10
-for x in range(numberofB - 4):
-    brick.drawbrick(xpos, 410, 75, 50, 70)
-    xpos += width + SPACE
-
-xpos = SPACE + width * 3 + 15
-for x in range(numberofB - 6):
-    brick.drawbrick(xpos, 380, 75, 120, 120)
-    xpos += width + SPACE
-
-xpos = SPACE + width * 4 + 20
-for x in range(numberofB - 8):
-    brick.drawbrick(xpos, 350, 0, 255, 0)
-    xpos += width + SPACE
-
+# sets red green and blue for brick color to a set ammount
+r = 255
+g = 10
+b = 12
+# starting y position
+ypos = 470
+# starting x position
+xpos = SPACE_BETWEEN_BRICKS
+# height of brick
+height = 25
+# loop that updated the number of brick, x-y position and color for each row of bricks
+for q in range(5):
+    # loop that creates a row of bricks
+    for x in range(numberofB):
+        brick.drawbrick(xpos, ypos, r, g, b)
+        xpos += width + SPACE_BETWEEN_BRICKS
+    ypos -= height + SPACE_BETWEEN_BRICKS
+    xpos = (SPACE_BETWEEN_BRICKS + width) * q + SPACE_BETWEEN_BRICKS + SPACE_BETWEEN_BRICKS + width
+    numberofB -= 2
+    r = 60 * q
+    g = 80 * q
+    b = 80 * q
+# loop that runs pygame untill it is exited out by user
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
